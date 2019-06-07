7a52e35abbaa9492e7b1e812bd4402b132a07d8c
Lindsey <lindsey.baker0@gmail.com>
2019-06-07 15:16:10 -0700

    Add name search click function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { DoctorSearch } from "./doctor-search.js";
+import {
+  DoctorSearch
+} from "./doctor-search.js";
 import $ from 'jquery';
 
 $(document).ready(function() {
@@ -13,16 +15,37 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      body.data.forEach(function(doctor){
+      body.data.forEach(function(doctor) {
         console.log(`${doctor.profile.first_name}`);
         $(".listDoctors").append(
 
           `<li>${doctor.profile.first_name} ${doctor.profile.last_name}</li>`
-          );
+        );
       })
     }, function(error) {
       $('.showErrors').text('There was an error processing your request: ${error.message}');
     });
+  });
 
+  $("#searchName").click(function(event) {
+    event.preventDefault();
+    let name = $('#name').val();
+    $('#name').val("");
+
+    let doctorSearch = new DoctorSearch();
+    let promise = doctorSearch.getDoctorListByName(name);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      body.data.forEach(function(doctor) {
+        console.log(`${doctor.profile.first_name}`);
+        $(".listDoctors").append(
+
+          `<li>${doctor.profile.first_name} ${doctor.profile.last_name}</li>`
+        );
+      })
+    }, function(error) {
+      $('.showErrors').text('There was an error processing your request: ${error.message}');
+    });
   });
 });
