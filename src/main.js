b2371a0e628e6319513bdc2eed78b51576fab725
Lindsey <lindsey.baker0@gmail.com>
2019-06-07 16:57:27 -0700

    Add forEach loop for practices, additional infomation now displaying on doctors

--- src/main.js
+++ src/main.js
@@ -20,14 +20,15 @@ $(document).ready(function() {
       }
       body.data.forEach(function(doctor) {
         console.log(`${doctor.profile.first_name}`);
-        $(".listDoctorsName").append(
-          `<li>Dr. ${doctor.profile.first_name} ${doctor.profile.last_name}</li>
-          <ul>
-            <li>Accepting New Patients:${body.data.practices.accepts_new_patients}</li>
-            <li>Website:${body.data.practices.website}</li>
-            <li>Address:${body.data.practices.visit_address.street} ${body.data.practices.visit_address.street2}<br>${body.data.practices.visit_address.city}, ${body.data.practices.visit_address.state}${body.data.practices.visit_address.zip}</li>
-            <li>Phone:${body.data.practices.phone.number}</li>
-            </ul>`);
+        doctor.practices.forEach(function(practice){
+          $(".listDoctorsName").append(
+            `<li>Dr. ${doctor.profile.first_name} ${doctor.profile.last_name}</li>
+            <ul>
+              <li>Accepting New Patients:${practice.accepts_new_patients}</li>
+              <li>Website:${practice.website}</li>
+              <li>Address:${practice.visit_address.street} ${practice.visit_address.street2}<br>${practice.visit_address.city}, ${practice.visit_address.state}${practice.visit_address.zip}</li>
+              </ul>`);
+        })
       })
     }, function(error) {
       $('.showErrors').text('There was an error processing your request: ${error.message}');
@@ -45,18 +46,19 @@ $(document).ready(function() {
     promise.then(function(response) {
       let body = JSON.parse(response);
       if(body.data.length === 0){
-        $(".listDoctorsName").append(`<li>No doctor's match your search</li>`);
+        $(".listDoctors").append(`<li>No doctor's match your search</li>`);
       }
       body.data.forEach(function(doctor) {
         console.log(`${doctor.profile.first_name}`);
-        $(".listDoctorsName").append(
-          `<li>Dr. ${doctor.profile.first_name} ${doctor.profile.last_name}</li>
-          <ul>
-            <li>Accepting New Patients:${body.data.practices.accepts_new_patients}</li>
-            <li>Website:${body.data.practices.website}</li>
-            <li>Address:${body.data.practices.visit_address.street} ${body.data.practices.visit_address.street2}<br>${body.data.practices.visit_address.city}, ${body.data.practices.visit_address.state}${body.data.practices.visit_address.zip}</li>
-            <li>Phone:${body.data.practices.phone.number}</li>
-            </ul>`);
+        doctor.practices.forEach(function(practice){
+          $(".listDoctorsName").append(
+            `<li>Dr. ${doctor.profile.first_name} ${doctor.profile.last_name}</li>
+            <ul>
+              <li>Accepting New Patients:${practice.accepts_new_patients}</li>
+              <li>Website:${practice.website}</li>
+              <li>Address:${practice.visit_address.street} ${practice.visit_address.street2}<br>${practice.visit_address.city}, ${practice.visit_address.state}${practice.visit_address.zip}</li>
+              </ul>`);
+        })
       })
     }, function(error) {
       $('.showErrors').text('There was an error processing your request: ${error.message}');
