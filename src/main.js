4298c2487aa043604154d317cd880f7335f9ae13
Lindsey <lindsey.baker0@gmail.com>
2019-06-07 12:41:56 -0700

    Append doctor's full name in an list item

--- src/main.js
+++ src/main.js
@@ -2,21 +2,20 @@ import { DoctorSearch } from "./doctor-search.js";
 import $ from 'jquery';
 
 $(document).ready(function() {
-  $("form").on("submit", function(event) {
+
+  $("form").click(function(event) {
     event.preventDefault();
-    let symptom = $("#symptom").val();
-    $("#symptom").val("");
 
     let doctorSearch = new DoctorSearch();
     let promise = doctorSearch.getDoctorListBySymptom(symptom);
 
     promise.then(function(response) {
-
-    let body = JSON.parse(response);
-    body.data.forEach(function())
-
-
-
+      let body = JSON.parse(response);
+      body.data.forEach(function(doctor){
+        $(".listDoctors").append(
+          `<li>${doctor.profile.first_name} ${doctor.profile.last_name}</li>`
+          );
+      })
     }, function(error) {
       $('.showErrors').text('There was an error processing your request: ${error.message}');
     });
