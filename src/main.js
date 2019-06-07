14d0d869f40cb9b31368fe2efbc83561b017d525
Lindsey <lindsey.baker0@gmail.com>
2019-06-07 11:13:42 -0700

    Add on submit function

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { DoctorSearch } from "./doctor-search.js";
+
+$(document).ready(function() {
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    let symptom = $("#symptom").val();
+    $("#symptom").val("");
+
+    let doctorSearch = new DoctorSearch();
+    let promise = doctorSearch.getDoctorListBySymptom(city);
+
+    // promise.then(function(response) {
+    //   let body = JSON.parse(response);
+    //   $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+    //   $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees`);
+    // }, function(error) {
+    //   $('.showErrors').text('There was an error processing your request: ${error.message}');
+    // });
+
+  });
+});
