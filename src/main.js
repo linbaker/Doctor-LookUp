c5b9103d10e87a85bc96b7d3f8aa470a599dd07a
Lindsey <lindsey.baker0@gmail.com>
2019-06-07 11:31:01 -0700

    Add error handling function

--- src/main.js
+++ src/main.js
@@ -9,10 +9,13 @@ $(document).ready(function() {
     let doctorSearch = new DoctorSearch();
     let promise = doctorSearch.getDoctorListBySymptom(city);
 
-    // promise.then(function(response) {
-    //   let body = JSON.parse(response);
-    //   $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-    //   $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees`);
+    promise.then(function(response) {
+
+      let body = JSON.parse(response);
+      body.data.foreACH
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees`);
+
     }, function(error) {
       $('.showErrors').text('There was an error processing your request: ${error.message}');
     });
