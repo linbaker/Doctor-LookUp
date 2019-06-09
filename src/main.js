bc63e34a445f77fd74b06449e7708c343c85f980
Lindsey <lindsey.baker0@gmail.com>
2019-06-09 10:45:19 -0700

    Add div in forEach append

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import {
-  DoctorSearch
-} from "./doctor-search.js";
+import { DoctorSearch } from "./doctor-search.js";
 import $ from 'jquery';
+import './styles.css';
+
 
 $(document).ready(function() {
 
   $("#searchSymptom").click(function(event) {
-    $(".listDoctors").empty();
+    $("#listDoctors").empty();
 
     event.preventDefault();
     let symptom = $('#symptom').val();
@@ -19,19 +19,19 @@ $(document).ready(function() {
     promise.then(function(response) {
       let body = JSON.parse(response);
       if(body.data.length === 0){
-        $(".listDoctors").html(`<li>No doctor's match your search</li>`);
+        $("#listDoctors").html(`<li>No doctor's match your search</li>`);
       }
       body.data.forEach(function(doctor) {
         console.log(`${doctor.profile.first_name}`);
         doctor.practices.forEach(function(practice){
-          $(".listDoctorsName").append(
-            `<li>Dr. ${doctor.profile.first_name} ${doctor.profile.last_name}</li>
+          $("#listDoctors").append(`<div class="square">
+            <li>Dr. ${doctor.profile.first_name} ${doctor.profile.last_name}</li>
             <ul>
               <li>Accepting New Patients:${practice.accepts_new_patients}</li>
               <li>Website:${practice.website}</li>
               <li>Address:${practice.visit_address.street} ${practice.visit_address.street2}<br>${practice.visit_address.city}, ${practice.visit_address.state}${practice.visit_address.zip}</li>
               <li>Phone:${practice.phones.number}</li>
-              </ul>`);
+              </ul></div>`);
         })
       })
     }, function(error) {
@@ -40,7 +40,7 @@ $(document).ready(function() {
   });
 
   $("#searchName").click(function(event) {
-    $(".listDoctorsName").empty();
+    $("#listDoctorsName").empty();
 
     event.preventDefault();
     let name = $('#name').val();
@@ -52,12 +52,12 @@ $(document).ready(function() {
     promise.then(function(response) {
       let body = JSON.parse(response);
       if(body.data.length === 0){
-        $(".listDoctorsName").html(`<li>No doctor's match your search</li>`);
+        $("#listDoctorsName").html(`<li>No doctor's match your search</li>`);
       }
       body.data.forEach(function(doctor) {
         console.log(`${doctor.profile.first_name}`);
         doctor.practices.forEach(function(practice){
-          $(".listDoctorsName").append(
+          $("#listDoctorsName").append(
             `<li>Dr. ${doctor.profile.first_name} ${doctor.profile.last_name}</li>
             <ul>
               <li>Accepting New Patients:${practice.accepts_new_patients}</li>
